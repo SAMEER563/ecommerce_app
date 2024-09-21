f4efc309ebe2d1fa6f8949626d67106f4f02e037
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-09-21 21:49:45 +0530

    Product page done

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,9 +1,100 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import { ShopContext } from '../context/shopContext';
+import { assets } from '../assets/assets';
+import RelatedProducts from '../components/RelatedProducts';
 
 const Product = () => {
-  return (
-    <div>Product</div>
-  )
+
+  const {productId} = useParams();
+ const {products,currency} = useContext(ShopContext);
+
+ const [productData, setProductData] = useState(false);
+ const [image, setImage] = useState('');
+ const [size,setSize] = useState([]);
+
+const fetchProductData = async () => {
+   products.map((item)=> {
+    if(item._id === productId) {
+      setProductData(item)
+     setImage(item.images[0])
+      return null;
+    }
+   })
+}
+
+useEffect(() => {
+  fetchProductData()
+},[productId,products])
+
+  return productData ? (
+    <div className='border-t-2 pt-10 transition-opacity ease-in-out duration-500 opacity-100'>
+       {/* Product Data */}
+       <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
+         
+         {/* Product Image */}
+         <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
+           <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
+           {
+              productData.image.map((item,index)=> (
+                <img onClick={()=>setImage(item)} src={item} key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' alt="" />
+              ))
+           }
+           </div>
+           <div className='w-full sm:w-[80%] '>
+        <img className='w-full h-auto' src={image} alt="" />
+           </div>
+         </div>
+          
+          {/*----------- Product Details--------- */}
+        <div className='flex-1'>
+          <h1 className='font-medium text-2xl mt-2'>{productData.name}</h1>
+          <div className='flex items-center gap-1 mt-2'>
+           <img src={assets.star_icon} alt="" className='w-3.5'/>
+           <img src={assets.star_icon} alt="" className='w-3.5'/>
+           <img src={assets.star_icon} alt="" className='w-3.5'/>
+           <img src={assets.star_icon} alt="" className='w-3.5'/>
+           <img src={assets.star_dull_icon} alt="" className='w-3.5'/>
+          <p className='pl-2'>(122)</p>
+          </div>
+          <p className='mt-3 text-3xl font-medium'>{currency}{productData.price}</p>
+          <p className='mt-3 text-gray-500 md:w-3/4'>{productData.description}</p>
+          <div className='flex flex-col gap-4 my-8'>
+           <p>Select Size</p>
+           <div className='flex gap-2'>
+            {productData.sizes.map((item,index)=>(
+             <button onClick={()=>setSize(item)} key={index} className={`border bg-gray-300 px-4 py-2 ${item === size ? 'border-orange-400' : ''}`}>{item}</button>
+            ))}
+           </div>
+          </div>
+          <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>Add To Cart</button>
+         <hr className='mt-8 sm:w-4/5'/>
+         <div>
+          <p>100% Original product.</p>
+          <p>cash on delivery is available on this product.</p>
+          <p>Easy return and exchange policy within 7 days.</p>
+         </div>
+        </div>
+       </div>
+
+       {/*  ----------Description & Review Section-------- */}
+       <div className='mt-20'>
+        <div className='flex'>
+          <b className='border px-5 py-3 text-sm'>Description</b>
+          <p className='border px-5 py-3 text-sm'>Reviews (122)</p>
+        </div>
+        <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
+          <p>An e-commerce website is an online plateform that facilitates the buying and selling of products or services over the internet.</p>
+          <p>E-commerce websites typically display products or services along with detailed(price, size,colors). Each product ususally has its own dedicated page with relevant information.</p>
+        </div>
+       </div>
+
+        {/*  ----------Related Products-------- */}
+
+       <RelatedProducts category={productData.category} subCategory={productData.subCategory} />
+
+    </div>
+  ) : <div className='opacity-0'>Loading...</div>
 }
 
 export default Product

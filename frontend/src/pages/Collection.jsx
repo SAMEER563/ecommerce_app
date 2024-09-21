fac5d35a21307d50ebace8d04276aa250a5fdba0
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-09-21 21:47:45 +0530

    collection with search

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -6,7 +6,7 @@ import ProductItem from '../components/ProductItem'
 
 const Collection = () => {
 
-const {products} = useContext(ShopContext);
+const {products,search, showSearch} = useContext(ShopContext);
 const [showFilter, setShowFilter] = useState(false);
 const [filterProducts,setFilterProducts] = useState([]);
 const [category,setCategory] = useState([]);
@@ -31,6 +31,10 @@ const toggleSubCategory = (e) => {
 
 const applyFilter = () => {
   let productsCopy = products.slice();
+
+  if(showSearch && search) {
+    productsCopy = productsCopy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()))
+  }
 
   if(category.length >0) {
     productsCopy= productsCopy.filter(item => category.includes(item.category))
@@ -63,11 +67,11 @@ const sortProduct = () => {
 
 useEffect(()=>{
   applyFilter()
-},[category,subCategory])
+},[category,subCategory, search, showSearch])
 
 
 useEffect(()=>{
-  sortProduct()
+  // sortProduct()   // it should be sortProduct not applyFilter
 },[sortType])
 
 

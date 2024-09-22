38fb62094601ec54b3ccb99858af74aaf38a52cb
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-09-22 16:45:37 +0530

    AddToCart functionality add

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -7,7 +7,7 @@ import RelatedProducts from '../components/RelatedProducts';
 const Product = () => {
 
   const {productId} = useParams();
- const {products,currency} = useContext(ShopContext);
+ const {products,currency,addToCart} = useContext(ShopContext);
 
  const [productData, setProductData] = useState(false);
  const [image, setImage] = useState('');
@@ -67,7 +67,7 @@ useEffect(() => {
             ))}
            </div>
           </div>
-          <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>Add To Cart</button>
+          <button onClick={()=>addToCart(productData._id,size)} className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>Add To Cart</button>
          <hr className='mt-8 sm:w-4/5'/>
          <div>
           <p>100% Original product.</p>

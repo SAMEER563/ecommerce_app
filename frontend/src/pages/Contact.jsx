e78254310b6202bf193d3ecd378e569987247101
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-09-25 09:52:54 +0530

    change

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -3,6 +3,8 @@ import Title from '../components/Title'
 import { assets } from '../assets/assets'
 import NewsletterBox from '../components/NewsletterBox'
 
+// hjgi
+
 const Contact = () => {
   return (
     <div>
